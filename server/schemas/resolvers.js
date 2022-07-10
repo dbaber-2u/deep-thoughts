4e3561966f8854658f68d1507ae6eb1c1b8aa3a1
Baber <dnbaber@hotmail.com>
2022-07-09 20:28:07 -0400

    implement JWT

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,9 +1,21 @@
 const { User, Thought } = require('../models');
 const { AuthenticationError } = require('apollo-server-express');
-
+const { signToken } = require('../utils/auth');
 
 const resolvers = {
   Query: {
+    me: async (parent, args, context) => {
+      if (context.user) {
+        const userData = await User.findOne({ _id: context.user._id })
+          .select('-__v -password')
+          .populate('thoughts')
+          .populate('friends');
+    
+        return userData;
+      }
+    
+      throw new AuthenticationError('Not logged in');
+    },
     // get all users
     users: async () => {
       return User.find()
@@ -29,23 +41,25 @@ const resolvers = {
   Mutation: {
     addUser: async (parent, args) => {
       const user = await User.create(args);
-    
-      return user;
+      const token = signToken(user);
+
+      return { token, user };
     },
     login: async (parent, { email, password }) => {
       const user = await User.findOne({ email });
-    
+
       if (!user) {
         throw new AuthenticationError('Incorrect credentials');
       }
-    
+
       const correctPw = await user.isCorrectPassword(password);
-    
+
       if (!correctPw) {
         throw new AuthenticationError('Incorrect credentials');
       }
-    
-      return user;
+
+      const token = signToken(user);
+      return { token, user };
     }
   }
 };
