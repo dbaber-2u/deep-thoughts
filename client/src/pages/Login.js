d59933422a1d7f58d61f94a8ebaef6cd2e47a3cb
Baber <dnbaber@hotmail.com>
2022-07-10 14:44:01 -0400

    establish token storage and use w/ graphql.

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import { useMutation } from '@apollo/client';
 import { LOGIN_USER } from '../utils/mutations';
+import Auth from '../utils/auth';
 
 
 const Login = (props) => {
@@ -10,7 +11,7 @@ const Login = (props) => {
   // update state based on form input changes
   const handleChange = (event) => {
     const { name, value } = event.target;
-    
+
     setFormState({
       ...formState,
       [name]: value,
@@ -20,13 +21,13 @@ const Login = (props) => {
   // submit form
   const handleFormSubmit = async event => {
     event.preventDefault();
-  
+
     try {
       const { data } = await login({
         variables: { ...formState }
       });
-  
-      console.log(data);
+
+      Auth.login(data.login.token);
     } catch (e) {
       console.error(e);
     }
